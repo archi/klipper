d0d4a5a09278059555c2ec8a232cc9799fd00fda
Sebastian Meyer <ich@sebmeyer.de>
2018-01-24 05:25:53 +0100

    adapt gpio and adc to stm32f1x3

--- src/stm32f1xx/gpio.c
+++ src/stm32f1xx/gpio.c
@@ -10,7 +10,8 @@
 #include "command.h" // shutdown
 #include "compiler.h" // ARRAY_SIZE
 #include "gpio.h" // gpio_out_setup
-#include "sam3x8e.h" // Pio
+#include "libmaple/gpio.h"
+#include "libmaple/adc.h"
 #include "sched.h" // sched_shutdown
 
 
@@ -18,50 +19,36 @@
  * Pin mappings
  ****************************************************************/
 
-#define GPIO(PORT, NUM) (((PORT)-'A') * 32 + (NUM))
-#define GPIO2PORT(PIN) ((PIN) / 32)
-#define GPIO2BIT(PIN) (1<<((PIN) % 32))
+#define GPIO(PORT, NUM) (((PORT)-'A') * 16 + (NUM))
+#define GPIO2PORT(PIN) ((PIN) / 16)
+#define GPIO2BIT(PIN) (1<<((PIN) % 16))
 
-static Pio * const digital_regs[] = {
-    PIOA, PIOB, PIOC, PIOD
-};
-
+static gpio_dev* digital_regs[4];
 
 /****************************************************************
  * General Purpose Input Output (GPIO) pins
  ****************************************************************/
 
 void
-gpio_peripheral(char bank, uint32_t bit, char ptype, uint32_t pull_up)
-{
-    Pio *regs = digital_regs[bank - 'A'];
-    if (ptype == 'A')
-        regs->PIO_ABSR &= ~bit;
-    else
-        regs->PIO_ABSR |= bit;
-    if (pull_up)
-        regs->PIO_PUER = bit;
-    else
-        regs->PIO_PUDR = bit;
-    regs->PIO_PDR = bit;
+gpio_init_stm32 (void) {
+    //GPIO{A,B,C,D} are no compile time constants
+    digital_regs[0] = GPIOA;
+    digital_regs[1] = GPIOB;
+    digital_regs[2] = GPIOC;
+    digital_regs[3] = GPIOD;
 }
-
+DECL_INIT(gpio_init_stm32);
 
 struct gpio_out
 gpio_out_setup(uint8_t pin, uint8_t val)
 {
     if (GPIO2PORT(pin) >= ARRAY_SIZE(digital_regs))
         goto fail;
-    Pio *regs = digital_regs[GPIO2PORT(pin)];
-    uint32_t bit = GPIO2BIT(pin);
+    gpio_dev *regs = digital_regs[GPIO2PORT(pin)];
+    uint8_t bit = GPIO2BIT(pin);
     irqstatus_t flag = irq_save();
-    if (val)
-        regs->PIO_SODR = bit;
-    else
-        regs->PIO_CODR = bit;
-    regs->PIO_OER = bit;
-    regs->PIO_OWER = bit;
-    regs->PIO_PER = bit;
+    gpio_set_mode(regs, bit, GPIO_OUTPUT_OD); //open drain (OD) or push-pull (PP)?
+    gpio_write_bit(regs, bit, val);
     irq_restore(flag);
     return (struct gpio_out){ .regs=regs, .bit=bit };
 fail:
@@ -71,18 +58,13 @@ fail:
 void
 gpio_out_toggle(struct gpio_out g)
 {
-    Pio *regs = g.regs;
-    regs->PIO_ODSR ^= g.bit;
+    gpio_toggle_bit(g.regs, g.bit);
 }
 
 void
 gpio_out_write(struct gpio_out g, uint8_t val)
 {
-    Pio *regs = g.regs;
-    if (val)
-        regs->PIO_SODR = g.bit;
-    else
-        regs->PIO_CODR = g.bit;
+    gpio_write_bit(g.regs, g.bit, val);
 }
 
 
@@ -91,17 +73,10 @@ gpio_in_setup(uint8_t pin, int8_t pull_up)
 {
     if (GPIO2PORT(pin) >= ARRAY_SIZE(digital_regs))
         goto fail;
-    uint32_t port = GPIO2PORT(pin);
-    Pio *regs = digital_regs[port];
-    uint32_t bit = GPIO2BIT(pin);
+    gpio_dev *regs = digital_regs[GPIO2PORT(pin)];
+    uint8_t bit = GPIO2BIT(pin);
     irqstatus_t flag = irq_save();
-    PMC->PMC_PCER0 = 1 << (ID_PIOA + port);
-    if (pull_up)
-        regs->PIO_PUER = bit;
-    else
-        regs->PIO_PUDR = bit;
-    regs->PIO_ODR = bit;
-    regs->PIO_PER = bit;
+    gpio_set_mode(regs, bit, pull_up ? GPIO_INPUT_PU : GPIO_INPUT_FLOATING); //or PD instead of FLOATING? 
     irq_restore(flag);
     return (struct gpio_in){ .regs=regs, .bit=bit };
 fail:
@@ -111,8 +86,8 @@ fail:
 uint8_t
 gpio_in_read(struct gpio_in g)
 {
-    Pio *regs = g.regs;
-    return !!(regs->PIO_PDSR & g.bit);
+    //TODO this cast uint32_t to uint8_t!
+    return gpio_read_bit(g.regs, g.bit);
 }
 
 
@@ -120,15 +95,42 @@ gpio_in_read(struct gpio_in g)
  * Analog to Digital Converter (ADC) pins
  ****************************************************************/
 
+//STM32F103x8 and STM32F103xB
 static const uint8_t adc_pins[] = {
-    GPIO('A', 2), GPIO('A', 3), GPIO('A', 4), GPIO('A', 6),
-    GPIO('A', 22), GPIO('A', 23), GPIO('A', 24), GPIO('A', 16),
-    GPIO('B', 12), GPIO('B', 13), GPIO('B', 17), GPIO('B', 18),
-    GPIO('B', 19), GPIO('B', 20)
+    GPIO('A', 0), GPIO('A', 1), GPIO('A', 2), GPIO('A', 3), //ADC12_IN{0,1,2,3}
+    GPIO('A', 4), GPIO('A', 5), GPIO('A', 6), GPIO('A', 7), //ADC12_IN{4,5,6,7}
+    GPIO('B', 0), GPIO('B', 1), //ADC12_IN{8,9}
+    GPIO('C', 0), GPIO('C', 1), GPIO('C', 2), GPIO('C', 3), //ADC12_IN{10,11,12,13}
+    GPIO('C', 4), GPIO('C', 5), //ADC12_IN{14,15}
 };
 
-#define ADC_FREQ_MAX 20000000
-DECL_CONSTANT(ADC_MAX, 4095);
+struct adc_status_t {
+    adc_dev* adc;
+    uint8_t channel; //=255 -> free
+};
+
+struct adc_status_t adc_status[]; 
+
+#define ADC_FREQ_MAX 14000000
+DECL_CONSTANT(ADC_MAX, (1<<12)-1);
+
+void
+adc_init_stm32(void)
+{
+    //ADC1 and ADC2 are no compile time constants
+    adc_status[0].adc=ADC1;
+    adc_status[0].channel=255;
+    adc_enable_single_swstart(ADC1);
+    adc_set_sample_rate(ADC1, ADC_SMPR_1_5);
+    adc_set_reg_seqlen(ADC1, 1);
+    
+    adc_status[1].adc=ADC2;
+    adc_status[1].channel=255;
+    adc_enable_single_swstart(ADC2);
+    adc_set_sample_rate(ADC2, ADC_SMPR_1_5);
+    adc_set_reg_seqlen(ADC2, 1);
+}
+DECL_INIT(adc_init_stm32);
 
 struct gpio_adc
 gpio_adc_setup(uint8_t pin)
@@ -141,16 +143,13 @@ gpio_adc_setup(uint8_t pin)
         if (adc_pins[chan] == pin)
             break;
     }
+    
+    gpio_dev *regs = digital_regs[GPIO2PORT(pin)];
+    uint8_t bit = GPIO2BIT(pin);
+    //for the stm32f1, the ADC is ignored in adc_config_gpio
+    adc_config_gpio(NULL, regs, bit);
 
-    if (!(PMC->PMC_PCSR1 & (1 << (ID_ADC-32)))) {
-        // Setup ADC
-        PMC->PMC_PCER1 = 1 << (ID_ADC-32);
-        uint32_t prescal = SystemCoreClock / (2 * ADC_FREQ_MAX) - 1;
-        ADC->ADC_MR = (ADC_MR_PRESCAL(prescal)
-                       | ADC_MR_STARTUP_SUT768
-                       | ADC_MR_TRANSFER(1));
-    }
-    return (struct gpio_adc){ .bit = 1 << chan };
+    return (struct gpio_adc){.chan=chan};
 }
 
 // Try to sample a value. Returns zero if sample ready, otherwise
@@ -159,21 +158,32 @@ gpio_adc_setup(uint8_t pin)
 uint32_t
 gpio_adc_sample(struct gpio_adc g)
 {
-    uint32_t chsr = ADC->ADC_CHSR & 0xffff;
-    if (!chsr) {
-        // Start sample
-        ADC->ADC_CHER = g.bit;
-        ADC->ADC_CR = ADC_CR_START;
-        goto need_delay;
+
+    for(int i = 0; i < 2; ++i) {
+        struct adc_status_t* adc = &adc_status[i];
+        if (adc->channel == g.chan) {
+            if (adc->adc->regs->SR & ADC_SR_EOC)
+                return 0;
+            else
+                goto need_delay;
+        }
     }
-    if (chsr != g.bit)
-        // Sampling in progress on another channel
+
+    struct adc_status_t* adc = NULL;
+    for(int i = 0; i < 2; ++i) {
+        struct adc_status_t* adc_test = &adc_status[i];
+        if (adc_test->channel == 255) {
+            adc = adc_test;
+            break;
+        }
+    }
+
+    if (!adc)
         goto need_delay;
-    if (!(ADC->ADC_ISR & ADC_ISR_DRDY))
-        // Conversion still in progress
-        goto need_delay;
-    // Conversion ready
-    return 0;
+
+    adc->adc->regs->SQR3 = g.chan;
+    adc->adc->regs->CR2 |= ADC_CR2_SWSTART;
+    adc->channel = g.chan;    
 need_delay:
     return ADC_FREQ_MAX * 1000ULL / CONFIG_CLOCK_FREQ;
 }
@@ -182,16 +192,30 @@ need_delay:
 uint16_t
 gpio_adc_read(struct gpio_adc g)
 {
-    ADC->ADC_CHDR = g.bit;
-    return ADC->ADC_LCDR;
+    for(int i = 0; i < 2; ++i) {
+        struct adc_status_t* adc = &adc_status[i];
+        if (adc->channel == g.chan) {
+            if (adc->adc->regs->SR & ADC_SR_EOC) {
+                uint16_t val = (uint16_t)(adc->adc->regs->DR & ADC_DR_DATA);
+                adc->channel = 255;
+                return val;
+            } else
+                goto error;
+        }
+    }
+error:
+    shutdown("Tried sampling too early, or no ADC assigned to pin!");
 }
 
 // Cancel a sample that may have been started with gpio_adc_sample()
 void
 gpio_adc_cancel_sample(struct gpio_adc g)
 {
-    irqstatus_t flag = irq_save();
-    if ((ADC->ADC_CHSR & 0xffff) == g.bit)
-        gpio_adc_read(g);
-    irq_restore(flag);
+    for(int i = 0; i < 2; ++i) {
+        struct adc_status_t* adc = &adc_status[i];
+        if (adc->channel == g.chan) {
+            adc->channel = 255;
+            //maybe we need to wait for the channel to finish sampling
+        }
+    }
 }
--- src/stm32f1xx/gpio.h
+++ src/stm32f1xx/gpio.h
@@ -1,27 +1,33 @@
-#ifndef __SAM3X8E_GPIO_H
-#define __SAM3X8E_GPIO_H
+#ifndef __STM32F1XX_GPIO_H
+#define __STM32F1XX_GPIO_H
 
 #include <stdint.h>
+
+//forward declarations
+struct gpio_dev;
+struct adc_dev;
 
 void gpio_peripheral(char bank, uint32_t bit, char ptype, uint32_t pull_up);
 
 struct gpio_out {
-    void *regs;
-    uint32_t bit;
+    struct gpio_dev *regs;
+    uint8_t bit;
 };
+
 struct gpio_out gpio_out_setup(uint8_t pin, uint8_t val);
 void gpio_out_toggle(struct gpio_out g);
 void gpio_out_write(struct gpio_out g, uint8_t val);
 
 struct gpio_in {
-    void *regs;
-    uint32_t bit;
+    struct gpio_dev *regs;
+    uint8_t bit;
 };
 struct gpio_in gpio_in_setup(uint8_t pin, int8_t pull_up);
 uint8_t gpio_in_read(struct gpio_in g);
 
 struct gpio_adc {
-    uint32_t bit;
+    struct adc_dev *adc;
+    uint8_t chan;
 };
 struct gpio_adc gpio_adc_setup(uint8_t pin);
 uint32_t gpio_adc_sample(struct gpio_adc g);
